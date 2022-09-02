7d06e5e3cdb0eebb683672405e0349e4349584cc
Charlie Kong <charlie@undercurrent.tech>
2022-09-02 10:49:02 +0800

    include deduct app call

--- js/app_call.js
+++ js/app_call.js
@@ -148,6 +148,14 @@ const deleteApp = async (account, appId) => {
 
     // read global state
     await readGlobalState(appId);
+
+    // call application to deduct counter
+    let deductAppArgs = [];
+    addAppArgs.push(new Uint8Array(Buffer.from("Deduct")));
+    await callApp(sender, appId, deductAppArgs);
+
+    // read global state
+    await readGlobalState(appId);
   } catch (error) {
     console.log(error.response.text);
 
